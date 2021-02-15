9cbe88ba4f6b436397b11ba1c1aef0951477a246
Sportsguy <mahoward2s@gmail.com>
2021-02-15 14:57:47 -0600

    added code to create DB

--- db/schema.sql
+++ db/schema.sql
@@ -6,6 +6,6 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(30),
-    devoured --boolean
+    devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUE ("Hamburger");
+INSERT INTO burgers (burger_name) VALUE ("Cheeseburger");
+INSERT INTO burgers (burger_name) VALUE ("Bacon Cheeseburger");
+INSERT INTO burgers (burger_name) VALUE ("Double Cheeseburger");
