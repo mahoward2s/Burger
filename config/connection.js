0ce93c13265b64a6dcbe793d72c55f59cb11997f
Sportsguy <mahoward2s@gmail.com>
2021-02-17 13:03:37 -0600

    added generic code as place holders

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
   user: 'root',
   // NOTE: Be sure to add your MySQL password here!
   password: 'password',
-  database: 'cat_db',
+  database: 'burgers_db',
 });
 
 // Make connection.
