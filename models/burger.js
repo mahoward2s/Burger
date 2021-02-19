09d21b140f672ecfbdeb708a30d566bfcd15c7aa
Sportsguy <mahoward2s@gmail.com>
2021-02-19 08:38:47 -0600

    made changes to variable names, added back the delete function

--- models/burger.js
+++ models/burger.js
@@ -2,15 +2,15 @@
 const orm = require('../config/orm.js');
 
 const burger = {
-  all(cb) {
-    orm.all('burgers', (res) => cb(res));
+  selectAll(cb) {
+    orm.selectAll('burgers', (res) => cb(res));
   },
   // The variables cols and vals are arrays.
-  create(cols, vals, cb) {
-    orm.create('burgers', cols, vals, (res) => cb(res));
+  insertOne(cols, vals, cb) {
+    orm.insertOne('burgers', cols, vals, (res) => cb(res));
   },
-  update(objColVals, condition, cb) {
-    orm.update('burgers', objColVals, condition, (res) => cb(res));
+  updateOne(objColVals, condition, cb) {
+    orm.updateOne('burgers', objColVals, condition, (res) => cb(res));
   },
 };
 
