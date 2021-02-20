0e23b903c9ae090f4935775ae766898a107bd36d
Sportsguy <mahoward2s@gmail.com>
2021-02-20 13:00:01 -0600

    heroku app issues...

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  // NOTE: Be sure to add your MySQL password here!
-  password: 'Password',
-  database: 'burgers_db',
-});
+if (process.env.JAWSBD_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    // NOTE: Be sure to add your MySQL password here!
+    password: '',
+    database: 'burgers_db',
+  });
+}
 
 // Make connection.
 connection.connect((err) => {
